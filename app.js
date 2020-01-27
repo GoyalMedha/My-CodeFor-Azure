0a3f7986b64d3c968f130b210097ec47715d1d36
unknown <medha.goyal1992@gmail.com>
2020-01-27 11:00:36 +0000

    Deployment-App Insight

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Start application insight by registering it
+var appInsights =require('applicationinsights');
+appInsights.setup('0aabf9e1-652d-4e57-84bc-cf5c7b71651c');
+appInsights.start();
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,6 +29,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.user('/problem',function()
+{
+throw new Error ("Oops!! Something went wrong!!")
+
+
+
+})
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -32,6 +48,8 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  appInsights.defaultClient.trackException({exception : err});
 
   // render the error page
   res.status(err.status || 500);
