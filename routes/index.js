5627306360d90722257274ccc02858723167d1c6
unknown <medha.goyal1992@gmail.com>
2020-01-27 09:37:11 +0000

    Staging Deployment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
-var express = require('Azure');
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Azure' });
+  //res.render('index', { title: 'Azure' });
+  var model = {
+      title : 'Azure',
+      message : process.env.MESSAGE || "This is Development Environment!!"
+  };
+  res.render('index',model);
 });
 
 module.exports = router;
